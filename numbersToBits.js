3dd118a8703d3b58749cd2e05f65352a6d54c7ad
Isaac Campos <42210213+srIsaac@users.noreply.github.com>
2020-03-07 16:44:41 -0300

    Added bootstrap

---
+++ numbersToBits.js
@@ -0,0 +1,27 @@
+let botaoNumbersToBits = $('#botaoNumbersToBits');
+let numeroConvertido = $('#numeroConvertido');
+let inputNumero = $('#inputNumero');
+
+botaoNumbersToBits.addEventListener('click', function(event){
+    
+    event.preventDefault();
+
+    let valorInputNumero = inputNumero.value;
+    
+    let arrayValorBit = [];
+
+    for(let i = 1; i <= valorInputNumero; i = i * 2){
+         arrayValorBit.push(i);
+    }
+
+    let arrayConvertido = arrayValorBit.reverse().map(function(item){
+        if(valorInputNumero >= item){
+            valorInputNumero = valorInputNumero - item;
+            return 1;
+        }
+        return 0;
+    });
+
+    arrayConvertido = arrayConvertido.join('');
+    numeroConvertido.textContent = arrayConvertido;
+});
